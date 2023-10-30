c0d1f6c9cd66a16edfb0ee5e281ff24bff615268
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-10-30 17:51:42 +0900

    feat: set up fonts file and installed navigation packages

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
+
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/helper';
+
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <Text
+        style={{
+          fontSize: 70,
+          // fontWeight: '700',
+          fontFamily: getFontFamily('Inter', '700'),
+        }}>
+        Font Check
+      </Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
