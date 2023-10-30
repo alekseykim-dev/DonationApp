e097823777d845eb01bb67064b5f074e74f65ea2
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-10-30 18:16:03 +0900

    feat: prepared globalStyles, scaling, routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {getFontFamily} from './assets/fonts/helper';
-
+import MainNavigation from './navigation/MainNavigation';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text
-        style={{
-          fontSize: 70,
-          // fontWeight: '700',
-          fontFamily: getFontFamily('Inter', '700'),
-        }}>
-        Font Check
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   );
 };
 
