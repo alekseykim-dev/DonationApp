9e28eb49ded6f886a8c93e2eb60f8ba08dfbf640
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-10-31 10:21:25 +0900

    feat: header component with switch and proptypes

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import style from './style';
 import {SafeAreaView, Text, View} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
+import Header from '../../components/Header/Header';
 
 const Home = () => {
   return (
     <SafeAreaView style={(globalStyle.backgroundWhite, globalStyle.flex)}>
-      <View>
-        <Text>Check</Text>
-      </View>
+      <Header title={'Donation App'} type={1} />
+      {/* <Header title={'Donation App'} type={2} />
+      <Header title={'Donation App'} type={3} /> */}
     </SafeAreaView>
   );
 };
---
+++ components/Header/style.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../../assets/fonts/helper';
+
+const style = StyleSheet.create({
+  title1: {
+    fontFamily: 'Inter',
+    fontSize: 24,
+    lineHeight: 29,
+    fontWeight: '600',
+  },
+  title2: {
+    fontFamily: 'Inter',
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: '600',
+  },
+  title3: {
+    fontFamily: 'Inter',
+    fontSize: 16,
+    lineHeight: 19,
+    fontWeight: '600',
+  },
+});
+
+export default style;
