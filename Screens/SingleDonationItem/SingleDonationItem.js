6e84b1b724abc6733b81fa0126b05622cf6ef893
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-11-07 17:14:07 +0900

    feat: building back button component

--- Screens/SingleDonationItem/SingleDonationItem.js
+++ Screens/SingleDonationItem/SingleDonationItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {SafeAreaView, ScrollView} from 'react-native';
+import BackButton from '../../components/BackButton/BackButton';
 import style from './style';
+import globalStyle from '../../assets/styles/globalStyle';
 
-const SingleDonationItem = () => {
+const SingleDonationItem = ({navigation}) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
-  console.log(donationItemInformation);
+  //   console.log(donationItemInformation);
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SingleDonationItem;
