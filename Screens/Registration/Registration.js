8501ff21ad11ef92769af9b1d2f06e58d70bb569
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-11-10 11:09:30 +0900

    feat: firebase auth install and create user in registration page

--- Screens/Registration/Registration.js
+++ Screens/Registration/Registration.js
@@ -7,7 +7,9 @@ import Input from '../../components/Input/Input';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import BackButton from '../../components/BackButton/BackButton';
+import {createUser} from '../../api/user';
 const Registration = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,6 +24,15 @@ const Registration = ({navigation}) => {
         contentContainerStyle={style.container}>
         <View style={globalStyle.marginBottom24}>
           <Header type={1} title={'Hello and Welcome!'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType={'default'}
+            label={'Full name'}
+            placeholder={'Enter your full name...'}
+            onChangeText={value => setFullName(value)}
+            // secureTextEntry={false}
+          />
         </View>
         <View style={globalStyle.marginBottom24}>
           <Input
@@ -41,7 +52,10 @@ const Registration = ({navigation}) => {
           />
         </View>
         <View style={globalStyle.marginBottom24}>
-          <Button title="Registration" />
+          <Button
+            title="Registration"
+            onPress={async () => await createUser(fullName, email, password)}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
