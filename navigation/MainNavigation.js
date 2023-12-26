0f42d3811c763df09c47cf8d1bcdc285622559f3
Aleksey Kim <alexkim@Alekseys-MacBook-Pro.local>
2023-12-26 12:08:29 +0900

    login

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,9 +1,9 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Routes';
-import Home from '../screens/Home/Home';
-import SingleDonationItem from '../screens/SingleDonationItem/SingleDonationItem';
-import Login from '../screens/Login/Login';
-import Registration from '../screens/Registration/Registration';
+import Home from '../Screens/Home/Home';
+import SingleDonationItem from '../Screens/SingleDonationItem/SingleDonationItem';
+import Login from '../Screens/Login/Login';
+import Registration from '../Screens/Registration/Registration';
 
 
 const Stack = createStackNavigator();
@@ -11,8 +11,8 @@ const Stack = createStackNavigator();
 const MainNavigation = () => {
   return (
     <Stack.Navigator screenOptions={{header: () => null, headerShown: false}}>
-      <Stack.Screen name={Routes.Login} component={Login} />
-      <Stack.Screen name={Routes.Registration} component={Registration} />
+      {/* <Stack.Screen name={Routes.Login} component={Login} />
+      <Stack.Screen name={Routes.Registration} component={Registration} /> */}
       <Stack.Screen name={Routes.Home} component={Home} />
       <Stack.Screen
         name={Routes.SingleDonationItem}
