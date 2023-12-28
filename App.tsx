9e68dd1d47f9279d7147a5168277ad2e0560b8ba
Aleksey Kim <alexkim@Alekseys-MacBook-Pro.local>
2023-12-28 17:27:12 +0900

    feat: token update with firebase and app state

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {AppState} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import RootNavigation from './navigation/RootNavigation';
 import store, {persistor} from './redux/store';
+import {checkToken} from './api/user';
 const App = () => {
+  const appState = useRef(AppState.currentState);
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      'change',
+      async nextAppState => {
+        if (
+          appState.current.match(/inactive|background/) &&
+          nextAppState === 'active'
+        ) {
+          console.log('You have come back into the app');
+          await checkToken();
+
+          //we are coming from background to foreground
+        }
+        checkToken();
+        appState.current = nextAppState;
+      },
+    );
+    console.log('App has been rendered');
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
